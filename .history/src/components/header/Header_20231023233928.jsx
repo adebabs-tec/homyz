57de47e1b4326ca17ef8dc0f51eda8e092c4e2a4
adebabs-tec <ruqoyatomotayo@gmail.com>
2023-10-23 23:45:22 +0100

    new commit

---
+++ .history/src/components/header/Header_20231023233928.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <section className="h-wrapper">
+      <div className="h-container">
+        <img src="./logo.png" alt="logo" width={100} />
+      </div>
+    </section>
+  )
+}
+
+export default Header
