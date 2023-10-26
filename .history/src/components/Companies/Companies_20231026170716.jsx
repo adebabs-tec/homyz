3f305b9c7736ff17240982035718b6541c0b8cf5
adebabs-tec <ruqoyatomotayo@gmail.com>
2023-10-26 17:10:41 +0100

    companies added

---
+++ .history/src/components/Companies/Companies_20231026170716.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Companies.css'
+
+const Companies = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="flexCenter innerWidth paddings c-container">
+        <img src="./" alt="" />
+      </div>
+    </section>
+  )
+}
+
+export default Companies
