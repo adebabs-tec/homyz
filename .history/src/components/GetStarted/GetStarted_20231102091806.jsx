eae9ff6e139cda094b9e6058cf48e0e8e63df637
adebabs-tec <ruqoyatomotayo@gmail.com>
2023-11-02 09:25:14 +0100

    getStarted added

---
+++ .history/src/components/GetStarted/GetStarted_20231102091806.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './GetStarted.css'
+
+const GetStarted = () => {
+  return (
+    <section className="g-wrapper">
+      <div className="paddings innerWidth g-container"></div>
+    </section>
+  )
+}
+
+export default GetStarted
